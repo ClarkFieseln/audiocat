27bc07eb8fed8c9cf5344b5f61b3f860b3b5650b
Clark Fieseln <54716944+ClarkFieseln@users.noreply.github.com>
2024-08-11 11:41:52 +0200

    Update mmrx.sh

--- audiocat_clark/mmrx.sh
+++ audiocat_clark/mmrx.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
-source rx.src | python3 mmrx.py $1
 
+# ask for password
+while true
+do
+    while true
+    do
+        echo -n "password: "
+        read -ers PASSWORD1
+        echo ""
+        # 2> /dev/null shall prevent showing the password if some error occurs
+        if [ ! "${PASSWORD1}" == "" ] 2> /dev/null
+        then
+            break
+        else
+            echo "Password cannot be empty!"
+        fi
+    done
+    echo -n "confirm password: "
+    read -ers PASSWORD
+    echo ""
+    # 2> /dev/null shall prevent showing the password if some error occurs
+    [ "${PASSWORD1}" == "${PASSWORD}" ] 2> /dev/null && break
+    echo "Passwords dont match!"
+done
+
+# RX
+source rx.src | python3 mmrx.py "${PASSWORD}" $1
